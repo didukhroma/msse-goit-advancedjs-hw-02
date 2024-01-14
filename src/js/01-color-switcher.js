ab8d586ac3145cb410b7d7a508c0febd375bad16
didukhroma <didukhroma@gmail.com>
2024-01-14 20:46:46 +0200

    build: task one done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const ref = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  timerId: null,
+};
+
+const changeBgColor = color => (ref.body.style.background = color);
+
+const handleClickStart = e => {
+  ref.timerId = setInterval(() => changeBgColor(getRandomHexColor()), 1000);
+};
+
+const handleClickStop = e => {
+  if (!ref.timerId) return;
+  clearInterval(ref.timerId);
+};
+
+ref.startBtn.addEventListener('click', handleClickStart);
+ref.stopBtn.addEventListener('click', handleClickStop);
